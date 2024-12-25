1509c04c93a150fda0fa80cad12dfd59a8a3f3ee
vitthalgarad143 <garadvitthal30@gmail.com>
2024-12-25 22:20:00 +0530

    new

---
+++ src/main/java/com/gl/vitthal/BatchprocessingApplication.java
@@ -0,0 +1,13 @@
+package com.gl.vitthal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchprocessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchprocessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gl/vitthal/BatchprocessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gl.vitthal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchprocessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
